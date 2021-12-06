f998bde7acdbaa51504ab56330b8ac3c82262117
David Burrell <d.burrell698@gmail.com>
2021-12-06 16:34:10 -0500

    complete song list component to fetch and display list of songs

--- client/index.js
+++ client/index.js
@@ -2,14 +2,21 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import ApolloClient from 'apollo-client';
 import { ApolloProvider } from 'react-apollo';
+import { Router, Route, hashHistory, IndexRoute } from 'react-router'
 
+import App from './components/App'
+import SongList from './components/SongList';
 
 const client = new ApolloClient({})
 
 const Root = () => {
   return (
     <ApolloProvider client = { client }>
-      <div>Lyrical</div>
+      <Router history={hashHistory}>
+        <Route path='/' component={App} >
+          <IndexRoute component={SongList} />
+        </Route>
+      </Router>
     </ApolloProvider>
   ) 
 };
