65c061197c91521ee1cb16112438207b91d2f14c
David Burrell <d.burrell698@gmail.com>
2021-12-09 12:38:06 -0500

    created add lyric to song funtionanlity to song details page

--- client/components/SongDetail.js
+++ client/components/SongDetail.js
@@ -1,13 +1,32 @@
 import React, { Component } from "react";
+import { Link } from 'react-router'
+import fetchSongDetails from "../queries/fetchSongDetails";
+import { graphql } from 'react-apollo'
+import LyricCreate from "./LyricCreate";
 
 class SongDetail extends Component {
+
+   
+    
+
     render(){
+
+        const { song } = this.props.data
+
+        if (!song) {
+            return <div>Loading...</div>
+        }
+
         return (
             <div>
-                <h3>Song Detail</h3>
+                <Link to='/' >Back</Link>
+                <h3>{song.title}</h3>
+                <LyricCreate songId={this.props.params.id} />
             </div>
         )
     }
 }
 
-export default SongDetail
+export default graphql(fetchSongDetails, { 
+    options: (props) => { return { variables: { id: props.params.id } } }
+ })(SongDetail)
